dfa6da639e6c1d37314c2fb4ceadf09be3a29459
Ben Barnes <ben.w.barnes@gmail.com>
2016-05-02 23:34:09 +1000

    Cleaned up some functions.

--- src/CORDIC.hs
+++ src/CORDIC.hs
@@ -4,23 +4,24 @@ import Data.Bits ( shift )
 import Numeric.Fixed
 
 -- Create a list of angles with tangent ratios [1,  1/(2^n)]
-angleDeltas :: Int -> [Fixed]
-angleDeltas n = [ toFixed $ atan ( 1 / 2 ^ e ) | e <- [ 0 .. n ] ]
+angleDeltas :: Int -> [Double]
+angleDeltas n = [ atan ( 1 / 2 ^ e ) | e <- [ 0 .. n ] ]
 
--- Generate a list of n + 1 scaling factors
--- Limited to 8 factors by fixed-point precision
-klist :: Int -> [Fixed]
-klist n = let l = min n 7 in
-    reverse $ foldr (\i ks -> k i * head ks : ks) [k 0] [l, l - 1 .. 1]
+-- An infinite list of scaling factors
+klist :: [Double]
+klist = klist' 1 ( k 0 )
+
+-- Recursive generator for scaling factors
+klist' :: Int -> Double -> [Double]
+klist' i n = n : klist' ( i + 1 ) (  k i * n )
 
 -- Scaling factor k at iteration i
-k :: Int -> Fixed
-k i = 1 / sqrt ( 1 + 1 / ( 2 ** ( 2 * fromIntegral i )))
+k :: Int -> Double
+k i = 1 / sqrt ( 1 +  2 ^^ (( -2 ) * i ))
 
 -- 'Matrix' multiplication step
-mult :: Int -> Fixed -> Fixed -> Fixed -> (Fixed, Fixed)
-mult i sign x y = let
-    shiftFixed n i = Fixed ( shift ( getFixed n ) i )
+mult :: Int -> Fixed -> (Fixed, Fixed) -> (Fixed, Fixed)
+mult i sign (x, y) = let
     sign' = if sign < 0
       then negate
       else id
@@ -28,3 +29,6 @@ mult i sign x y = let
     y' = sign' ( shiftFixed x ( -i )) + y
   in (x', y')
 
+-- Shift function which handles wrapping/unwrapping fixed-point values
+shiftFixed :: Fixed -> Int -> Fixed
+shiftFixed n i = Fixed ( shift ( getFixed n ) i )
